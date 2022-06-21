140795f0e464af574136852bf191053f2bd65765
bubasara <sarastankovicsarita@gmail.com>
2022-06-21 14:25:40 +0200

    how to play layout in progress

--- app/src/main/java/bubasara/quizypeasy/dialogs/HowToPlayDialog.kt
+++ app/src/main/java/bubasara/quizypeasy/dialogs/HowToPlayDialog.kt
@@ -1,6 +1,43 @@
 package bubasara.quizypeasy.dialogs
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.DialogFragment
+import bubasara.quizypeasy.R
+import bubasara.quizypeasy.databinding.DialogHowToPlayBinding
 
-class HowToPlayDialog : DialogFragment() {
+class HowToPlayDialog : DialogFragment(R.layout.dialog_how_to_play) {
+
+    private var _binding : DialogHowToPlayBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.full_screen_dialog)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DialogHowToPlayBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
