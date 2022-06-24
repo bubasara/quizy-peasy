ef59946fc75ff8eb924776a5623fd1e3135e67fb
bubasara <sarastankovicsarita@gmail.com>
2022-06-24 18:03:52 +0200

    nav graph created

--- app/src/main/java/bubasara/quizypeasy/activities/MainActivity.kt
+++ app/src/main/java/bubasara/quizypeasy/activities/MainActivity.kt
@@ -2,11 +2,22 @@ package bubasara.quizypeasy.activities
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import bubasara.quizypeasy.R
+import bubasara.quizypeasy.databinding.ActivityMainBinding
 
 class MainActivity : FragmentActivity() {
+
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_splashscreen)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+        setContentView(R.layout.item_category)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
