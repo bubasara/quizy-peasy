c2fc6f0870377a0e1ada3792581d1e1dc160242c
bubasara <sarastankovicsarita@gmail.com>
2022-06-24 18:03:14 +0200

    minor changes

---
+++ app/src/main/java/bubasara/quizypeasy/fragments/ItemGameplayFragment.kt
@@ -0,0 +1,13 @@
+package bubasara.quizypeasy.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import bubasara.quizypeasy.R
+
+class ItemGameplayFragment : Fragment(R.layout.item_gameplay) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
