e6c260e59d1484f190b1945f9d7cb9305972b968
bubasara <sarastankovicsarita@gmail.com>
2022-06-24 18:03:40 +0200

    category item created

---
+++ app/src/main/java/bubasara/quizypeasy/fragments/ItemCategoryFragment.kt
@@ -0,0 +1,13 @@
+package bubasara.quizypeasy.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import bubasara.quizypeasy.R
+
+class ItemCategoryFragment : Fragment(R.layout.item_category) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
