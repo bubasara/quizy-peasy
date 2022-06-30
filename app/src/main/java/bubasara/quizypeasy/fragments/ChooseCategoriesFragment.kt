cc9a5098dab2a1a364d623e48a77c253cba43d42
bubasara <sarastankovicsarita@gmail.com>
2022-06-30 17:20:27 +0200

    check-uncheck category implemented

--- app/src/main/java/bubasara/quizypeasy/fragments/ChooseCategoriesFragment.kt
+++ app/src/main/java/bubasara/quizypeasy/fragments/ChooseCategoriesFragment.kt
@@ -5,11 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import bubasara.quizypeasy.R
+import bubasara.quizypeasy.adapters.ChooseCategoriesAdapter
 import bubasara.quizypeasy.databinding.FragmentChooseCategoriesBinding
+import bubasara.quizypeasy.viewmodels.ChooseCategoriesViewModel
+import bubasara.quizypeasy.viewmodels.SharedViewModel
 
 class ChooseCategoriesFragment : Fragment(R.layout.fragment_choose_categories) {
+
+    val viewModel : ChooseCategoriesViewModel by viewModels()
+    val sharedViewModel : SharedViewModel by activityViewModels()
+
+    private lateinit var chooseCategoriesAdapter : ChooseCategoriesAdapter
+    private var hasCategoryCheckStateChanged = false
+    var deletePosition = -1
 
     private var _binding : FragmentChooseCategoriesBinding? = null
     private val binding get() = _binding!!
@@ -26,8 +39,37 @@ class ChooseCategoriesFragment : Fragment(R.layout.fragment_choose_categories) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //todo long click on category -> delete dialog
+
         //todo click on category -> checked
+
+        viewModel.listOfCategories = sharedViewModel.listOfCategories
+
+        val recyclerViewCategories = binding.recyclerViewCategories
+
+        //  long click on category -> delete dialog
+        this.chooseCategoriesAdapter = ChooseCategoriesAdapter(requireContext(),
+            object : ChooseCategoriesAdapter.ChooseCategoriesAdapterInterface{
+                override fun longClickOnCategory(position: Int) {
+                    deletePosition = position
+                    findNavController().navigate(R.id.action_chooseCategoriesFragment_to_deleteCategoryDialog)
+                }
+
+                override fun clickOnIsChecked() {
+                    hasCategoryCheckStateChanged = true
+                }
+            }
+        )
+
+        //  data from view model is transferred to adapter
+        chooseCategoriesAdapter.setListOfCategories(viewModel.listOfCategories)
+        recyclerViewCategories.adapter = chooseCategoriesAdapter
+        recyclerViewCategories.layoutManager = LinearLayoutManager(context)
+
+        if(sharedViewModel.getNewCategory() != null) {
+            viewModel.listOfCategories.add(sharedViewModel.getNewCategory()!!)
+            chooseCategoriesAdapter.notifyDataSetChanged()
+            
+        }
 
         //click on Create new category button -> go to Create New Category fragment
         binding.btnCreateNewCategory.setOnClickListener {
