b1753a254377438387ec0a2120641e4bb6085d62
bubasara <sarastankovicsarita@gmail.com>
2022-07-05 17:09:32 +0200

    minor changes

--- app/src/main/java/bubasara/quizypeasy/models/Question.kt
+++ app/src/main/java/bubasara/quizypeasy/models/Question.kt
@@ -1,4 +1,4 @@
 package bubasara.quizypeasy.models
 
 //data class for question with answers and correct answer
-data class Question (var question : String, var listOfAnswers : ArrayList<String>, var correctAnswer : Int)
+data class Question (var question : String, var listOfAnswers : ArrayList<String>, var correctAnswer : String)
