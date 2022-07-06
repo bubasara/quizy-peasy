a40158c6b22c6afa613f38af01a906faf3c86142
bubasara <sarastankovicsarita@gmail.com>
2022-07-06 19:06:31 +0200

    entity created

--- app/src/main/java/bubasara/quizypeasy/models/Category.kt
+++ app/src/main/java/bubasara/quizypeasy/models/Category.kt
@@ -1,7 +1,25 @@
 package bubasara.quizypeasy.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 //data class for category with list of questions
-data class Category (var categoryName : String, val numberOfQuestions : Int, var isChecked : Boolean, val listOfQuestions : ArrayList<Question>, val imgCategory : Int){
+@Entity(tableName = "category")
+data class Category (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "category_name")
+    var categoryName : String,
+    @ColumnInfo(name = "number_of_questions")
+    val numberOfQuestions : Int,
+    @ColumnInfo(name = "is_checked")
+    var isChecked : Boolean,
+    @ColumnInfo(name = "list_of_questions")
+    val listOfQuestions : ArrayList<Question>,
+    @ColumnInfo(name = "img_category")
+    val imgCategory : Int)
+{
     fun addQuestion(question: Question){
         listOfQuestions.add(question)
     }
--- app/src/main/java/bubasara/quizypeasy/models/Question.kt
+++ app/src/main/java/bubasara/quizypeasy/models/Question.kt
@@ -1,4 +1,17 @@
 package bubasara.quizypeasy.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 //data class for question with answers and correct answer
-data class Question (var question : String, var listOfAnswers : ArrayList<String>, var correctAnswer : String)
+@Entity(tableName = "question")
+data class Question (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "question")
+    var question : String,
+    @ColumnInfo(name = "list_of_answers")
+    var listOfAnswers : ArrayList<String>,
+    @ColumnInfo(name = "correct_answer")
+    var correctAnswer : String)
