ab693c7a827dd8a57cdaa83d7036f38bde5e614a
bubasara <sarastankovicsarita@gmail.com>
2022-07-11 16:30:05 +0200

    database class created as signleton

---
+++ app/src/main/java/bubasara/quizypeasy/models/QuizyPeasyRoomDatabase.kt
@@ -0,0 +1,33 @@
+package bubasara.quizypeasy.models
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Category::class, Question::class, CategoryQuestionsDao::class], version = 1, exportSchema = false)
+abstract class QuizyPeasyRoomDatabase : RoomDatabase() {
+
+    abstract fun categoryDao(): CategoryDao
+    abstract fun questionDao(): QuestionDao
+    abstract fun categoryQuestionsDao(): CategoryQuestionsDao
+
+    /*  allows access to the methods for creating or getting the db
+        using the class name as the qualifier   */
+    companion object {
+        //  singleton because we need only one instance of the db
+        @Volatile
+        private var INSTANCE: QuizyPeasyRoomDatabase? = null
+
+        fun getDatabase(context: Context): QuizyPeasyRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext, QuizyPeasyRoomDatabase::class.java, "quizy_peasy_database"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
