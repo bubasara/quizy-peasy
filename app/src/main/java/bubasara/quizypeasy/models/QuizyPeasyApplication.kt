64a0e2c966e89c772357b853a2bda983342bdf40
bubasara <sarastankovicsarita@gmail.com>
2022-09-28 14:48:27 +0200

    PreferenceManager init

--- app/src/main/java/bubasara/quizypeasy/models/QuizyPeasyApplication.kt
+++ app/src/main/java/bubasara/quizypeasy/models/QuizyPeasyApplication.kt
@@ -1,9 +1,29 @@
 package bubasara.quizypeasy.models
 
 import android.app.Application
+import bubasara.quizypeasy.helpers.PreferenceManager
 import bubasara.quizypeasy.models.QuizyPeasyRoomDatabase
+import bubasara.quizypeasy.utils.getJsonDataFromAsset
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class QuizyPeasyApplication : Application() {
     //  instantiate the database
     val database : QuizyPeasyRoomDatabase by lazy { QuizyPeasyRoomDatabase.getDatabase(this) }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // init pref managera, citanje iz asseta i uspis u pref ako je pref prazan
+        PreferenceManager.preferenceManagerInit(this)
+        /*if(PreferenceManager.getListOfCategories().isEmpty())
+            setListOfCategoriesFromJson(this)*/
+    }
+
+
+/*    private fun setListOfCategoriesFromJson(application: Application){
+        val json = getJsonDataFromAsset(application, "data.json")
+        val list = object : TypeToken<List<Category>>() {}.type
+        PreferenceManager.setListOfCategories(Gson().fromJson(json, list))
+    }*/
 }
