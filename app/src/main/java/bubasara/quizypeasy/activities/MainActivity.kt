876f55b32dabfde70b4950caafd649a1154a3b89
bubasara <sarastankovicsarita@gmail.com>
2022-09-28 14:15:58 +0200

    minor changes

--- app/src/main/java/bubasara/quizypeasy/activities/MainActivity.kt
+++ app/src/main/java/bubasara/quizypeasy/activities/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import bubasara.quizypeasy.R
 import bubasara.quizypeasy.databinding.ActivityMainBinding
-import bubasara.quizypeasy.models.QuizyPeasyRoomDatabase
 
 class MainActivity : FragmentActivity() {
 
@@ -14,14 +13,9 @@ class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setContentView(R.layout.item_category)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-
-        /*//  instantiate the database
-        val database : QuizyPeasyRoomDatabase by lazy { QuizyPeasyRoomDatabase.getDatabase(this) }*/
     }
 }
