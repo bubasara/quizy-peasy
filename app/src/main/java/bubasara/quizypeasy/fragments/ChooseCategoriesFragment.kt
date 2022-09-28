88e29d1791378e3fab65fdcb4b5db50f3a865b8f
bubasara <sarastankovicsarita@gmail.com>
2022-09-28 14:47:41 +0200

    category data initialization

--- app/src/main/java/bubasara/quizypeasy/fragments/ChooseCategoriesFragment.kt
+++ app/src/main/java/bubasara/quizypeasy/fragments/ChooseCategoriesFragment.kt
@@ -7,26 +7,33 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import bubasara.quizypeasy.R
 import bubasara.quizypeasy.adapters.ChooseCategoriesAdapter
 import bubasara.quizypeasy.databinding.FragmentChooseCategoriesBinding
-import bubasara.quizypeasy.helpers.PreferenceManager
-import bubasara.quizypeasy.viewmodels.ChooseCategoriesViewModel
+import bubasara.quizypeasy.models.QuizyPeasyApplication
+import bubasara.quizypeasy.utils.getListOfCategoriesFromJson
+import bubasara.quizypeasy.viewmodels.CategoryViewModel
+import bubasara.quizypeasy.viewmodels.CategoryViewModelFactory
 import bubasara.quizypeasy.viewmodels.SharedViewModel
 
 class ChooseCategoriesFragment : Fragment(R.layout.fragment_choose_categories) {
 
-    private val viewModel : ChooseCategoriesViewModel by viewModels()
-    private val sharedViewModel : SharedViewModel by activityViewModels()
+    private val categoryViewModel: CategoryViewModel by viewModels {
+        CategoryViewModelFactory(
+            (activity?.application as QuizyPeasyApplication).database.categoryDao()
+        )
+    }
 
-    private lateinit var chooseCategoriesAdapter : ChooseCategoriesAdapter
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+
+    private lateinit var chooseCategoriesAdapter: ChooseCategoriesAdapter
     private var hasCategoryCheckStateChanged = false
     var deletePosition = -1
 
-    private var _binding : FragmentChooseCategoriesBinding? = null
+    private var _binding: FragmentChooseCategoriesBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -41,14 +48,11 @@ class ChooseCategoriesFragment : Fragment(R.layout.fragment_choose_categories) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.listOfCategories = sharedViewModel.listOfCategories
+        initCategoriesData()
 
-        val recyclerViewCategories = binding.recyclerViewCategories
+        chooseCategoriesAdapter = ChooseCategoriesAdapter(requireContext(),
 
-
-        this.chooseCategoriesAdapter = ChooseCategoriesAdapter(requireContext(),
-            object : ChooseCategoriesAdapter.ChooseCategoriesAdapterInterface{
-
+            object : ChooseCategoriesAdapter.ChooseCategoriesAdapterInterface {
                 //  long click on category -> delete dialog
                 override fun longClickOnCategory(position: Int) {
                     deletePosition = position
@@ -63,28 +67,37 @@ class ChooseCategoriesFragment : Fragment(R.layout.fragment_choose_categories) {
         )
 
         //  data from view model is transferred to adapter
-        chooseCategoriesAdapter.setListOfCategories(viewModel.listOfCategories)
-        recyclerViewCategories.adapter = chooseCategoriesAdapter
-        recyclerViewCategories.layoutManager = LinearLayoutManager(context)
+        chooseCategoriesAdapter.setListOfCategories(categoryViewModel.allCategoriesMutable.value ?: listOf())
+        binding.recyclerViewCategories.adapter = chooseCategoriesAdapter
+        binding.recyclerViewCategories.layoutManager = LinearLayoutManager(context)
 
-        if(sharedViewModel.getNewCategory() != null) {
+        // todo -> when new category is being added
+/*        if(sharedViewModel.getNewCategory() != null) {
             viewModel.listOfCategories.add(sharedViewModel.getNewCategory()!!)
             chooseCategoriesAdapter.notifyDataSetChanged()
 
             PreferenceManager.setListOfCategories(viewModel.listOfCategories)
             sharedViewModel.resetNewCategory()
-        }
+        }*/
+
 
         //  observer for deleting category
         sharedViewModel.categoryDeleteLiveDataBoolean.observe(viewLifecycleOwner) {
             if (it) {
-                chooseCategoriesAdapter.deleteItem(deletePosition)
-                PreferenceManager.setListOfCategories(viewModel.listOfCategories)
+
+                categoryViewModel.deleteCategory(categoryViewModel.allCategoriesMutable.value!![deletePosition])
+
+                //viewModel.allCategoriesMutable.value?.removeAt(deletePosition)
+                //chooseCategoriesAdapter.deleteItem(deletePosition)
+
+                //PreferenceManager.setListOfCategories(viewModel.listOfCategories)
                 chooseCategoriesAdapter.notifyItemRemoved(deletePosition)
                 chooseCategoriesAdapter.notifyItemRangeChanged(
                     deletePosition,
-                    viewModel.listOfCategories.size
+                    categoryViewModel.allCategoriesMutable.value?.size ?: 1
                 )
+
+                sharedViewModel.categoryDeleteLiveDataBoolean.value = false
             }
         }
 
@@ -100,11 +113,33 @@ class ChooseCategoriesFragment : Fragment(R.layout.fragment_choose_categories) {
         }
     }
 
+    private fun initCategoriesData() {
+        //  if there is no records of categories in db
+        categoryViewModel.retrieveAllCategories().observe(this.viewLifecycleOwner) { allCategories ->
+
+            if(allCategories.isNullOrEmpty())
+            {
+                for (categoryWithQuestions in getListOfCategoriesFromJson(requireContext())) {
+                    // insert categories from json
+                    categoryViewModel.addNewCategory(categoryWithQuestions.categoryName, categoryWithQuestions.numberOfQuestions, categoryWithQuestions.isChecked, categoryWithQuestions.imgCategory)
+                }
+            }
+
+            categoryViewModel.allCategoriesMutable = MutableLiveData(allCategories.toMutableList())
+            chooseCategoriesAdapter.setListOfCategories(allCategories)
+            chooseCategoriesAdapter.notifyDataSetChanged()
+        }
+    }
+
+
+
+
+
     override fun onPause() {
         super.onPause()
         //  if category checked/unchecked
         if (hasCategoryCheckStateChanged) {
-            PreferenceManager.setListOfCategories(viewModel.listOfCategories)
+            //PreferenceManager.setListOfCategories(viewModel.listOfCategories)
         }
     }
 
