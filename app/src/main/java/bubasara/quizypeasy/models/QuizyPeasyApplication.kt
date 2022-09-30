65d0ffcc3bfb5bb04023fa7543a5264972574511
bubasara <sarastankovicsarita@gmail.com>
2022-09-30 17:31:36 +0200

    minor changes

--- app/src/main/java/bubasara/quizypeasy/models/QuizyPeasyApplication.kt
+++ app/src/main/java/bubasara/quizypeasy/models/QuizyPeasyApplication.kt
@@ -2,10 +2,6 @@ package bubasara.quizypeasy.models
 
 import android.app.Application
 import bubasara.quizypeasy.helpers.PreferenceManager
-import bubasara.quizypeasy.models.QuizyPeasyRoomDatabase
-import bubasara.quizypeasy.utils.getJsonDataFromAsset
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 
 class QuizyPeasyApplication : Application() {
     //  instantiate the database
